182b7e63ec84c5f7b043cca794fcd6398f5167a2
yadong.zhang <yadong.zhang@pcitc.com>
2017-04-12 15:59:59 +0800

    按需求更改

--- ces/src/main/java/com/dapeng/ces/controller/PersistenceController.java
+++ ces/src/main/java/com/dapeng/ces/controller/PersistenceController.java
@@ -3,22 +3,34 @@ package com.dapeng.ces.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dapeng.ces.dto.UserParam;
 import com.dapeng.ces.dto.UserResult;
+import com.dapeng.ces.dto.UserScoreNewResult;
 import com.dapeng.ces.dto.UserScorePlayerResult;
 import com.dapeng.ces.model.Score;
 import com.dapeng.ces.model.UserScore;
 import com.dapeng.ces.service.persistence.PersistenceService;
+import com.dapeng.ces.util.StringUtil;
 
 @RestController
 public class PersistenceController {
     @Autowired
     private PersistenceService persistenceService;
+    @Value("${rsgene}")
+    private String rsgene;
+
+    @RequestMapping(value = "init",method = RequestMethod.GET,produces = "application/json")
+    public boolean init(){
+        persistenceService.saveUserGene();
+        persistenceService.saveScore();
+        persistenceService.saveUserScore();
+        return true;
+    }
     
     @RequestMapping(value = "saveUserGene",method = RequestMethod.GET,produces = "application/json")
     public List<UserResult> saveUserGene(){
@@ -35,16 +47,15 @@ public class PersistenceController {
         return persistenceService.saveUserScore();
     }
     
-    @RequestMapping(value = "userCompare",method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-    public List<UserScorePlayerResult> userCompare(@RequestBody UserParam userParam){
-//        List<String> list = new ArrayList<>();
-//        list.add("rs1815739");
-//        list.add("rs1803285");
-//        list.add("rs8192678");
-//        list.add("rs1799865");
-        if(userParam == null){
-            return null;
-        }
-        return persistenceService.userCompare(userParam.getUserName(), userParam.getList());
+    @RequestMapping(value = "getUserScore",method = RequestMethod.GET,produces = "application/json")
+    public List<UserScoreNewResult> getUserScore(@RequestParam("userName") String userName){
+        return persistenceService.getUserScore(userName);
+    }
+    
+    @RequestMapping(value = "userCompare",method = RequestMethod.GET,produces = "application/json")
+    public List<UserScorePlayerResult> userCompare(@RequestParam("userName") String userName){
+        //获取对比的位点
+        List<String> list = StringUtil.string2List(rsgene, ",");
+        return persistenceService.userCompare(userName, list);
     }
 }
