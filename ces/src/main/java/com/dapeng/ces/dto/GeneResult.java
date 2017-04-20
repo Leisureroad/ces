4be2d70e693d26ef72ec7de2a719c7aefe08574d
liufan <t.fanliu@pcitc.com>
2017-04-20 19:26:22 +0800

    加geneCode

--- ces/src/main/java/com/dapeng/ces/dto/GeneResult.java
+++ ces/src/main/java/com/dapeng/ces/dto/GeneResult.java
@@ -3,6 +3,7 @@ package com.dapeng.ces.dto;
 public class GeneResult {
 	private String name;
 	private String value;
+	private String code;
 	public String getName() {
 		return name;
 	}
@@ -15,13 +16,21 @@ public class GeneResult {
 	public void setValue(String value) {
 		this.value = value;
 	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("Gene [name=");
+		builder.append("GeneResult [name=");
 		builder.append(name);
 		builder.append(", value=");
 		builder.append(value);
+		builder.append(", code=");
+		builder.append(code);
 		builder.append("]");
 		return builder.toString();
 	}
